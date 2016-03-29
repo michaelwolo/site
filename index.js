c7141d0611d44586c6503ea8ea7b1775a5a7e90d
Michael Woloszuk <mwoloszuk@gmail.com>
2016-03-28 23:49:58 -0500

    Add comment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client')));
 
-// console.log((path.join(__dirname, '/client')));
+console.log((path.join(__dirname, '/client')));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
