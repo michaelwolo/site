5aa7f07098b0980ef64e5571bdb7acdd3c06073f
Michael Woloszuk <mwoloszuk@gmail.com>
2016-03-28 23:47:39 -0500

    Edit statuc file path

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.listen(8080, function () {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, 'client')));
+
+// console.log((path.join(__dirname, '/client')));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
