c593ac558671980cf4e2b80cf1458faa10bcc81c
Michael Woloszuk <mwoloszuk@gmail.com>
2016-03-28 23:35:45 -0500

    Move client and add second endpoint

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-app.listen(80, function () {
+app.listen(8080, function () {
   console.log('Your site is served. Bon appetit.')
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
+});
+
+app.get('/data', function (req, res) {
+  res.send({
+    data: 'some data'
+  });
 });
